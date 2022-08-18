b2f0d519137d0452e918ae86adad1f99faf6eb18
rajesh <krajeshgowri7032@mail.com>
2022-08-18 15:24:07 +0530

    Javascript-Selecthtml

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  var x = document.getElementById("sel").length;
+  document.body.style.backgroundColor="pink";
+  document.getElementById("demo").innerHTML=x;
+  
+  document.getElementsByClassName("opt").length;
+}
+
+
